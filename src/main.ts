b5f3d81a20c78b9066413fbaa076e6c145789f41
Yaseen Bhojani <yaseenbhojani2@gmail.com>
2023-07-10 22:55:26 +0500

    .

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,25 @@
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  // Create a Nest application instance
+  const app = await NestFactory.create(AppModule);
 
+  // Middleware: Parse cookies
   app.use(cookieParser());
-  // app.enableCors({
-  //   origin: 'https://spiceroutekitchen.netlify.app',
-  //   credentials: true, // Allow sending and receiving cookies across origins
-  // });
 
+  // Enable Cross-Origin Resource Sharing (CORS)
+  app.enableCors();
+
+  // Global validation pipe to validate incoming request payloads
   app.useGlobalPipes(new ValidationPipe());
 
-  const port = process.env.PORT || 3000;
-  await app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
-  });
+  // Start listening on the specified port (default: 3333)
+  await app.listen(3000);
 }
 
+// Bootstrap the Nest application
 bootstrap();
