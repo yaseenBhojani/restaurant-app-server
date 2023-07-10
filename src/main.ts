4539805a151984461648af4ee9e73ac25c8430ce
Yaseen Bhojani <yaseenbhojani2@gmail.com>
2023-07-10 22:02:59 +0500

    .

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import cookieParser from 'cookie-parser';
+import * as cookieParser from 'cookie-parser';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 
