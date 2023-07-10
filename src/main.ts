40fe2e83e3d099c936e63af80c210e4db129dc99
Yaseen Bhojani <yaseenbhojani2@gmail.com>
2023-07-10 23:17:13 +0500

    update

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,13 @@ async function bootstrap() {
   app.use(cookieParser());
 
   // Enable Cross-Origin Resource Sharing (CORS)
-  app.enableCors();
+  app.enableCors({
+    origin: ['https://spiceroutekitchen.netlify.app/', 'http://localhost:3000'],
+    methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
+    credentials: true,
+    allowedHeaders:
+      'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
+  });
 
   // Global validation pipe to validate incoming request payloads
   app.useGlobalPipes(new ValidationPipe());
