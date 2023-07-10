9ceb036bce4135038dc6adc250ed661098ea11e3
Yaseen Bhojani <yaseenbhojani2@gmail.com>
2023-07-10 22:19:47 +0500

    .

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import cookieParser from 'cookie-parser';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 import { AppModule } from './app.module';
 
@@ -11,10 +12,20 @@ async function bootstrap() {
   // Middleware: Parse cookies
   app.use(cookieParser());
 
-  // Enable Cross-Origin Resource Sharing (CORS)
-  app.enableCors({
-    origin: 'https://spiceroutekitchen.netlify.app',
-  });
+  // Reverse Proxy Configuration
+  const reverseProxyConfig = {
+    target: 'https://restaurant-app-server-chi.vercel.app',
+    changeOrigin: true,
+    headers: {
+      'Access-Control-Allow-Origin': 'https://spiceroutekitchen.netlify.app',
+      'Access-Control-Allow-Headers':
+        'Origin, X-Requested-With, Content-Type, Accept',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    },
+  };
+
+  // Enable Reverse Proxy
+  app.use('/auth', createProxyMiddleware(reverseProxyConfig));
 
   // Global validation pipe to validate incoming request payloads
   app.useGlobalPipes(new ValidationPipe());
